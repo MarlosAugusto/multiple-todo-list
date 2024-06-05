65f0e912f6ab78edbfcefa2809bbe9390f3c6132
MarlosAugusto <marlos.gomes@codengage.com>
2024-06-05 17:58:07 -0300

    refactor: improves homepage

--- app/controllers/todo_item_controller.rb
+++ app/controllers/todo_item_controller.rb
@@ -1,5 +1,29 @@
 class TodoItemController < ApplicationController
+  before_action :set_todo_item, except: [:index] #only: [:show, :edit, :update, :destroy]
+
   def index
-    @todo_items = TodoItem.all
+    @todo_items = TodoItem.sorted
+  
+    @types = ["personal", "professional"]
+    @situations = ["incomplete", "complete", "abandoned", "expired"]
+  end
+
+  def edit
+  end
+
+  def update
+    if @todo_item.update(todo_item_params)
+      redirect_to todo_item_path(@todo_item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def todo_item_params
+    params.require(:todo_item).permit(:title, :status, :description, :deadline, :abandonment_reason)
+  end
+
+  def set_todo_item
+    @todo_item = TodoItem.find(params[:id])
   end
 end
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -11,5 +11,40 @@ class TodoItem < ApplicationRecord
   has_rich_text :title
 
   validates :title, presence: true
-  validates :abandonment_reason, presence: true, if: Proc.new { |t| t.status == 2}
+  validates :abandonment_reason, presence: true, if: Proc.new { |t| t.status == "abandoned" }
+
+  scope :sorted, -> {
+    where(status: "incomplete")
+    .where("deadline >= ? or deadline IS NULL", Date.today)
+    .order(arel_table[:deadline].asc.nulls_last)
+  }
+  scope :incomplete, -> { where(status: "incomplete") }
+  scope :complete, -> { where(status: "complete") }
+  scope :abandoned, -> { where(status: "abandoned") }
+  scope :expired, -> { where.not(deadline: nil).where("deadline < ?", Date.today) }
+
+  scope :personal, -> { where(todo_item_type: "personal") }
+  scope :professional, -> { where(todo_item_type: "professional") }
+
+  #todo_item_type
+  def personal?
+    todo_item_type == "personal"
+  end
+  def professional?
+    todo_item_type == "professional"
+  end
+
+  #status
+  def expired?
+    deadline < Date.today && !completed?
+  end
+  def completed?
+    status == "complete"
+  end
+  def incompleted?
+    status == "incomplete"
+  end
+  def abandoned?
+    status == "abandoned"
+  end
 end
