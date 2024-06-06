5737e17628045becc88b2d61ee8650a72516e338
MarlosAugusto <marlos.gomes@codengage.com>
2024-06-06 09:23:51 -0300

    feat: adds stimulus-component scroll-reveal

---
+++ app/javascript/controllers/scroll_reveal_controller.js
@@ -0,0 +1,27 @@
+import ScrollReveal from '@stimulus-components/scroll-reveal'
+
+export default class extends ScrollReveal {
+  connect() {
+    super.connect()
+    console.log('Do what you want here.')
+  }
+
+  // Override this method to change the IntersectionObserver behavior
+  intersectionObserverCallback(entries, observer) {
+    //
+  }
+
+  // Options used for the IntersectionObserver
+  get intersectionObserverOptions() {
+    return {}
+  }
+
+  // You can override this getter to set your default options here.
+  get defaultOptions() {
+    return {
+      class: 'active',
+      threshold: 0.5,
+      rootMargin: '100px',
+    }
+  }
+}
