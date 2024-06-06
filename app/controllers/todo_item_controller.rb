98a661b9bd863308add65db6e578e5d90b447e86
MarlosAugusto <marlos.gomes@codengage.com>
2024-06-06 11:03:36 -0300

    feat: edit todo_item

--- app/controllers/todo_item_controller.rb
+++ app/controllers/todo_item_controller.rb
@@ -9,18 +9,26 @@ class TodoItemController < ApplicationController
   end
 
   def edit
+    @types = ["personal", "professional"]
   end
 
   def update
     if @todo_item.update(todo_item_params)
-      redirect_to todo_item_path(@todo_item)
+      redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
+  # def filter_by_status
+  #   @todo_items = TodoItem.all.where(geographic_filter: params[:geo_filter])
+  #                         .includes(:specialties, :addresses, :phone_numbers, :cabinets).shuffle
+
+  #   render partial: 'midwives/midwives_cards', locals: { midwives: midwives }
+  # end
+
   def todo_item_params
-    params.require(:todo_item).permit(:title, :status, :description, :deadline, :abandonment_reason)
+    params.require(:todo_item).permit(:title, :status, :description, :deadline, :abandonment_reason, :todo_item_type)
   end
 
   def set_todo_item
