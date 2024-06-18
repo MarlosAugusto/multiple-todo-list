a46ddbbf1d6dd83289817e04791ebb816369504a
MarlosAugusto <marlos.gomes@codengage.com>
2024-06-18 11:59:35 -0300

    refactor: reset application with esbuild

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,5 +1,5 @@
 import { Application } from "@hotwired/stimulus"
-
+// import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 const application = Application.start()
 
 // Configure Stimulus development experience
